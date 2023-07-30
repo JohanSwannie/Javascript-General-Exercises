5d801ad550657526ff8361cf9d04d2feab61cf1a
jswan <jswannieman@gmail.com>
2023-07-31 09:45:20 +1200

    Update ge10 Javascript Component

--- ge10.js
+++ ge10.js
@@ -1,3 +1,13 @@
-console.log("Before debugger");
-debugger;
-console.log("After debugger");
+function outer() {
+  let name = "outer";
+  let str = inner();
+  return str;
+}
+function inner() {
+  let name = "inner";
+  return "Hello !";
+}
+console.log("before outer() call");
+// debugger;
+console.log(outer());
+console.log("after outer() call");
