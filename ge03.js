099f8694c282422cfea20ed2f5b024164fd55300
jswan <jswannieman@gmail.com>
2023-09-16 17:45:39 +1200

    Update ge03 Javascript Component

--- ge03.js
+++ ge03.js
@@ -1,10 +1,21 @@
 let isOver = false;
 while (isOver !== true) {
-  let randy = Math.floor(Math.random() * 100);
+  let combination = "";
+  let randy = Math.floor(Math.random() * 40);
   let continueLoop = confirm(
-    `Your Random Number is : ${randy} - Continue the loop?`
+    `Your Random Lotto Number is : ${randy} - Continue the loop?`
   );
   isOver = continueLoop === true ? false : true;
 }
 
-alert("Loop is OVER!");
+alert(
+  "You reached the end of your random number search! - Click Restart to search for numbers again"
+);
+
+const butter = document.getElementById("butty");
+
+butty.style.display = "block";
+
+butty.addEventListener("click", () => {
+  location.reload();
+});
